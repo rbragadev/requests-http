1dd6ca83f350c8dde4b6e169396672ca2234eef8
Raphael Braga <raffael.info@gmail.com>
2023-01-25 15:07:04 -0300

    aula 121 - create environment

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Curso } from './curso';
 import { tap } from 'rxjs';
+import { environment } from 'src/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ import { tap } from 'rxjs';
 export class CursosService {
   constructor(private http: HttpClient) {}
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   list() {
     return this.http.get<Curso[]>(this.API).pipe(tap(console.log));
